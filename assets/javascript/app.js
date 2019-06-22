583b1c0931349c921c50c02a2fce302900db47bb
Pjae215 <lala2462002@hotmail.com>
2019-06-22 01:31:21 -0400

    added comments and reset button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,21 @@
-var answers = [["1000", "100", "10", "1"], ["14", "72", "12", "36"], ["5", "6", "9", "3"], ["1", "5", "0", "50"], ["22", "34", "36", "24"]];
-var listOfCorrectAnswers = ["100", "12", "9", "0", "24"];
-var listOfQuestions = ["1. What is 10x10?", "2.What is the square root of 144? ", "3. What is 3 squared?", "4. What is 5x0?", "5. What is 72 divided by 3?"];
+// array of questions, choices and answers
+var choices = [["1000", "100", "1", "10"], ["14", "12", "72", "36"], ["5", "6", "9", "3"], ["1", "5", "0", "50"], ["22", "34", "36", "24"]];
+var rightanswers = ["100", "12", "9", "0", "24"];
+var listOfQuestions = ["1. What is 10x10?", "2.What is the square root of 144? ", "3. What is 3 squared?", "4. What is 5 x 0?", "5. What is 72 divided by 3?"];
 
-
+//assign variables for questions, choices, answers and totals
 var numQuestions = document.getElementsByClassName("Questions");
 var numAnswers = document.getElementsByClassName("Answers");
-var submitButton = document.getElementById("submitanswers");
 var questionElement = document.getElementById("Question");
 var questionCount = 0;
 var score = 0;
 
+//for restart
+function resetButton(){
+location.reload();
+}
+
+//call function for when start button is clicked...will hide image and button and show totals
 function startButton() {
 	//show the score
 	document.getElementById("score").innerHTML = score;
@@ -26,25 +32,26 @@ function startButton() {
 		showQuestionsAndAnswers();
 	}
 }
+//makes question/answer div visible 
 function showQuestionsAndAnswers() {
 	questionElement.innerHTML = listOfQuestions[questionCount];
 	document.getElementById("Question").style.visibility = "visible";
-	document.getElementById("1").innerHTML = answers[questionCount][0];
-	document.getElementById("2").innerHTML = answers[questionCount][1];
-	document.getElementById("3").innerHTML = answers[questionCount][2];
-	document.getElementById("4").innerHTML = answers[questionCount][3];
+	document.getElementById("1").innerHTML = choices[questionCount][0];
+	document.getElementById("2").innerHTML = choices[questionCount][1];
+	document.getElementById("3").innerHTML = choices[questionCount][2];
+	document.getElementById("4").innerHTML = choices[questionCount][3];
 	for(var i =0; i<numAnswers.length; i++) {
 		numAnswers[i].style.visibility = "visible";
 	}
 }
-
+//match index of questions/choices/answers from arrays
 function checkAnswer(number) {
 	questionCount++;
 		for(var i =0; i<numAnswers.length; i++) {
 				numAnswers[i].style.visibility = "false";
 			}
 		var answer = document.getElementById(number).innerHTML;
-		if (listOfCorrectAnswers.indexOf(answer) != -1) {
+		if (rightanswers.indexOf(answer) != -1) {
 			score ++;
 		}
 		document.getElementById("score").innerHTML = score;
@@ -53,7 +60,12 @@ function checkAnswer(number) {
 		document.getElementById(number).blur();
 	} else {
 		setTimeout(function() {
-			alert("You scored " + score + " out of 5.");
+			alert("You scored " + score + " out of 5.  Game Over!");
 		}, 5);
+		setTimeout(function() {
+			resetButton();
+		}, 5)
+	
 	}
 }
+
